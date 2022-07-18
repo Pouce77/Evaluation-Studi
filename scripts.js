560e48d8de54057c10ddcc93dc347bf80f2c1f19
Julien Kunze <julienkunze0@gmail.com>
2022-07-18 11:08:14 +0200

    Construction du footer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
+//click sur le bouton du carousel ayant l'Id : boutonActu.
 let button=document.getElementById('boutonActu');
 button.addEventListener("click",clickbtn);
-
 function clickbtn(){
-
     location.href='actualites.html';
 }
